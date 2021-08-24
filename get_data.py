55f8b0c20d16c973a5ff656118df88cd0442adae
madnanua <ronintoadin@gmail.com>
2021-08-25 00:24:06 +0700

    Update from mobile

--- get_data.py
+++ get_data.py
@@ -1,3 +1,10 @@
-import config
 
 
+from binance.client import Client
+client = Client(config.API_KEY, config.API_SECRET)
+
+acc_info = client.get_account()
+
+balance = acc_info['balances']
+
+print(balance)
